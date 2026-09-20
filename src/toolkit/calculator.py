b87ab0522e8ae90aa43280c3ab7bec9348fcd6b5
Iarkiy <iarkiy_08@mail.ru>
2026-09-21 01:02:08 +0300

    correct work of tokenizer ( -u patch)

--- src/toolkit/calculator.py
+++
@@ -1,5 +0,0 @@
-from tokenizer import rpn
-
-if __name__=="__main__":
-    s='123+3//345+5-3.4'
-    print(rpn(s))
--- src/toolkit/tokenizer.py
+++ src/toolkit/tokenizer.py
@@ -1,30 +1,33 @@
 import re
 
-def tokenize(chars):               #вводим токенизатор
+#pattern 
+def tokenize(chars):               
     tokens=[]
     pattern = re.compile(r'''
         (?P<NUMBER>\d+\.\d+|\d+)
       | (?P<MINUS>-)               
       | (?P<OPERATOR>//|[+*/%]+)           
       | (?P<SPACE>\s+)              
-      | (?P<MISMATCH>.)             #чтобы поднять ошибку для неверных символов
+      | (?P<MISMATCH>.)             
     ''', re.VERBOSE)
 
+    #typed tokens
     for token in pattern.finditer(chars):
         value = token.group()                   
-        type = token.lastgroup                 #типизация токена
+        type = token.lastgroup                 
         if type == 'SPACE':
-            continue                            #переход к следующей итерации
+            continue                            
         if type == 'MISMATCH':
             raise SyntaxError("Неверный символ")
-        tokens.append((value, type))                 # кортеж        
+        tokens.append((value, type))                       
     return tokens
 
-def rpn(tokens):
+#transforming_to_RPN
+def to_RPN(tokens):
     tokens=(tokenize(tokens))
     for char in range(len(tokens)):
-        if tokens[char][1] == "MINUS" and (char == 0 or tokens[char-1][1] == "OPERATOR"):
-            tokens[char] = (('-u',"UNARY MINUS")) 
+        if tokens[char][1] == "MINUS" and (char == 0 or tokens[char-1][1] == "OPERATOR"):           
+            tokens[char] = (('-u',"UNARY_MINUS")) 
 
     output=[]
     operators=[]
@@ -36,15 +39,13 @@ def rpn(tokens):
             output.append(value)
         else:
             while operators and ops[operators[-1]]>=ops[value]:
-                    output.append(operators.pop())
+                output.append(operators.pop())
             operators.append(value)
-
-
 
     while operators:
         output.append(operators.pop())   
     return output 
 
-s='-12*3+123//--123'
-print(rpn(s))
+s='+12*3+123//-123'
+print(to_RPN(s))
 
